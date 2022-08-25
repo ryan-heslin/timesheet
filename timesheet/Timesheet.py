d579839453b9bc8fe93162ab706f7e69fa3d591d
ryan-heslin <rh3788@american.edu>
2022-08-24 20:08:47 -0400

    Improve documentation

--- timesheet/Timesheet.py
+++ timesheet/Timesheet.py
@@ -2,8 +2,8 @@ import csv
 import datetime
 import json
 import shelve
-from functools import reduce
 from functools import lru_cache
+from functools import reduce
 from os import access
 from os import listdir
 from os import makedirs
@@ -23,7 +23,7 @@ time_list = Union[List[datetime.datetime], List[datetime.time], List["DiffTime"]
 
 
 class DiffTime(datetime.time):
-    """Subclass of datetime.time that supports intended to represent times without reference to a particular day. arithmetic by adding a dummy datetime.time attribute that contains hours, minutes, seconds, and microseconds components"""
+    """Subclass of :code:`datetime.time` intended to represent times without reference to a particular day. It supports subtraction by creating a dummy :code:`datetime.time` attribute that contains hour, minute, second, and microsecond components"""
 
     def __init__(
         self,
@@ -31,12 +31,12 @@ class DiffTime(datetime.time):
         minute: int = 0,
         second: int = 0,
         microsecond: int = 0,
-        tzinfo : datetime.tzinfo =None,
+        tzinfo: datetime.tzinfo = None,
         *,
-        fold : int=0,
+        fold: int = 0,
     ):
         """
-        Initialize a DiffTime object. This subclass of :code:`datetime.datetime` creates a dummy `datetime.time` object 
+        Initialize a DiffTime object. This subclass of :code:`datetime.datetime` creates a dummy `datetime.time` object
 
         :param hour int: Hour value in :code:`range(24)`
         :param minute int: Minute value in :code:`range(60)`
@@ -46,21 +46,27 @@ class DiffTime(datetime.time):
         :param fold int: 0 or 1 indicating whether to distinguish "wall times"
         """
         super().__init__()
-        self._date_impl = self.dummy_date(self)
+        self._date_impl = self._dummy_date(self)
 
     @classmethod
     def from_time(
         cls, time: Union["DiffTime", datetime.time, datetime.datetime]
     ) -> "DiffTime":
-        """Converts existing datetime.datetime or datetime.time object to DiffTime"""
+        """
+        Alternate constructor that converts an existing :code:`datetime.time` or :code:`datetime.datetime` instance to :code:`DiffTime`
+
+        :param cls [TODO:type]: [TODO:description]
+        :param time Union["DiffTime", datetime.time, datetime.datetime]: :code:`datetime.time` or :code:`datetime.datetime` instance
+        :rtype "DiffTime": :code:`DiffTime` instance
+        """
         instance = cls.__new__(
             cls, time.hour, time.minute, time.second, time.microsecond
         )
-        instance._date_impl = instance.dummy_date(instance)
+        instance._date_impl = instance._dummy_date(instance)
         return instance
 
     @staticmethod
-    def dummy_date(time: Union[datetime.datetime, "DiffTime"]) -> datetime.datetime:
+    def _dummy_date(time: Union[datetime.datetime, "DiffTime"]) -> datetime.datetime:
         """Creates a time datetime object with given year, day, and month fields set to dummy values"""
         return datetime.datetime(
             year=1,
@@ -74,30 +80,25 @@ class DiffTime(datetime.time):
 
     def __sub__(self, other: "DiffTime") -> datetime.timedelta:
         """Return timedelta object with difference"""
-        return self._date_impl - self.dummy_date(other)
+        return self._date_impl - __class__._dummy_date(other)
 
     @property
     @lru_cache()
     def min(self):
         """Lowest possible DiffTime object"""
-        return DiffTime(hour = 0, second = 0, minute = 0,  microsecond = 0 )
+        return DiffTime(hour=0, second=0, minute=0, microsecond=0)
 
     @property
     @lru_cache()
     def max(self):
         """Greatest possible DiffTime object"""
-        return DiffTime(hour = 23, minute = 59, second = 59,  microsecond = 999999 )
+        return DiffTime(hour=23, minute=59, second=59, microsecond=999999)
 
 
 class DayLog:
 
-    
     # Used to convert time differences to hours
-    hour_conversions = {
-            "days" : 24 ,
-            "seconds" : 1 / 3600, 
-            "microseconds" : 1 / 3.6e+9
-            }
+    hour_conversions = {"days": 24, "seconds": 1 / 3600, "microseconds": 1 / 3.6e9}
 
     def __init__(
         self,
@@ -105,7 +106,7 @@ class DayLog:
         timestamps: time_list = [],
     ) -> None:
         """
-        Initialize DayLog instance. This class records a series of timestamps within the 
+        Initialize DayLog instance. This class records a series of timestamps within the
         span of a specified date. It can form intervals from these timestamps and compute their total sum.
 
         :param date datetime.date: Date the instance should refer to
@@ -115,16 +116,26 @@ class DayLog:
         # If, no timestamps provided, don't automatically supply any
         self._validate_timestamp_sequence(timestamps, raise_exception=True)
         self.timestamps = self.convert_to_DiffTime(timestamps)
-        self.date = utils.handle_date_arg(date, default=datetime.date.today(), allow_None=True)
+        self.date = utils.handle_date_arg(
+            date, default=datetime.date.today(), allow_None=True
+        )
         self.creation_time = datetime.datetime.today()
 
     def concat_timestamps(self, timestamps: time_list = None) -> None:
+        """
+        Concatenate additional timestamps to those stored in the calling instance. Added timestamps must all be later than the last timestamp stored in the
+        caller and sorted in ascending order.
+
+        :param timestamps time_list: [TODO:description]
+        :rtype None:
+        :raises ValueError: If any member of :code:`timestamps` is later than the caller's latest timestamp and/or :code:`timestamps` is not sorted in ascending order.
+        """
         converted = (
             [datetime.datetime.today()]
             if timestamps is None or len(timestamps) == 0
             else timestamps
         )
-        converted= self.convert_to_DiffTime(converted)
+        converted = self.convert_to_DiffTime(converted)
         self._validate_timestamp_sequence(timestamps=converted, raise_exception=True)
 
         # New timestamp must come after all recorded ones
@@ -138,8 +149,13 @@ class DayLog:
         return len(self.timestamps)
 
     @staticmethod
-    def yyyymmdd(timestamp=datetime.datetime.today()):
-        """Format a date YYYY-MM-DD"""
+    def yyyymmdd(timestamp: Union[datetime.datetime, None] = None):
+        """
+        Format a date YYYY-MM-DD
+
+        :param timestamp Union[datetime.datetime,  None]: :code:`datetime.datetime` instance
+        """
+        timestamp = datetime.datetime.today() if timestamp is None else timestamp
         return datetime.datetime.strftime(timestamp, "%Y-%m-%d")
 
     def __str__(self) -> str:
@@ -150,8 +166,15 @@ class DayLog:
             + [str(x) for x in self.timestamps]
         )
 
-    def __add__(self, other: "DayLog"):
-        """Adding another DayLog object concatenates the two lists of timestamps in a new object (assuming the result is valid)"""
+    def __add__(self, other: "DayLog") -> "DayLog":
+        """
+        Combine this :code:`DayLog` instance with another by creating a new
+        instance with concatenated timestamps. Fails if the two instances do not describe the same date or the timstamps are incompatible.
+
+        :param other "DayLog": Another :code:`DayLog` instance. It must refer to the same date, and its timestamps, if they exist, must meet the requirements for :code:`DayLog.concat_timestamps`
+        :raises ValueError: If any of the above conditions are not met
+        :rtype "DayLog" A new instance with the concatenated timestamps
+        """
         if self.date != other.date:
             raise ValueError(
                 f"Cannot add DiffTime objects when dates {self.date} and {other.date} disagree"
@@ -164,13 +187,23 @@ class DayLog:
 
     @staticmethod
     def convert_to_hours(delta: datetime.timedelta) -> float:
-        """Converts a datetime.timedelta object to its decimal value in hours"""
+        """
+        Converts a :code:`datetime.timedelta` object to its value in hours.
+
+        :param delta datetime.timedelta: :code:`datetime.timedelta` instance
+        :rtype float: Value of the instance in hours
+        """
         return sum(
-                    conversion * getattr(delta, unit)
-                    for unit, conversion in __class__.hour_conversions.items()
-                )
+            conversion * getattr(delta, unit)
+            for unit, conversion in __class__.hour_conversions.items()
+        )
 
     def sum_time_intervals(self) -> float:
+        """
+        Sum this instance's time intervals. Returns the total value of the time contained in the intervals in hours. If there is an odd number of timestamps, leaving an unbounded interval, the last is ignored with a warning.
+
+        :rtype float: Total time spanned by all intervals stored in this instance, or 0 if fewer than two are recorded.
+        """
         n_timestamps = len(self)
 
         # If 0 or 1 timestamps only, no difference to account for
@@ -199,26 +232,42 @@ class DayLog:
         return out
 
     @staticmethod
-    def convert_to_DiffTime(timestamps: time_list):
+    def convert_to_DiffTime(timestamps: time_list) -> List["DiffTime"]:
+        """
+        Convert a list of timestamps to :code:`DiffTime` instances
+
+        :param timestamps time_list: List whose members are all instances of :code:`datetime.datetime`, :code:`datetime.time`, or :code:`DiffTime`
+        :rtype List[DiffTime] : List of :code:`DiffTime` instances
+        """
         return [
             DiffTime.from_time(x) if not isinstance(x, DiffTime) else x
             for x in timestamps
         ]
 
 
-
 class Timesheet:
+    """Contains a set of DayLog objects mapped to dates and implements methods for summarizing and aggregating hours worked across different units of time, such as days or months"""
+
     def __init__(
         self,
         data: Dict[str, DayLog] = None,
-        storage_path=None,
-        storage_name=None,
-        save=True,
-        json_path=None,
+        storage_path: str = None,
+        storage_name: str = None,
+        save: bool = True,
+        json_path : str =None,
     ) -> None:
+        """
+        Initialize a :code:`Timesheet` instance.
+
+        :param data Dict[str, DayLog]: Optional dict one mapping or more :code:`DayLog`  instances to :code:`str` keys, which must be recognizable as ISO-formatted dates
+        :param storage_path str: Optional path to directory in which to store this instance. Defaults to :code:`$HOME/.timesheets` 
+        :param storage_name : Optional name for this instance in the :code:`shelve` file in which it is stored. If already in use, an error is thrown.
+        :param save bool: Optional bool determining whether to save on instance creation
+        :param json_path [TODO:type]: [TODO:description]
+        :rtype None: 
+        """
 
         # If no initial data is supplied, default to empty DayLog dated today
-
         self._constructor(
             data=data,
             storage_path=storage_path,
@@ -245,21 +294,22 @@ class Timesheet:
         )
         # Generate default storage name if none provided
         if (arg_name := kwargs["storage_name"]) is None:
-            # Get storage names already in use, if any 
+            # Get storage names already in use, if any
+            overwrite = True
+            used_names = []
             if exists(self.storage_path):
-                used_names = utils.use_shelve_file(
+                used_names.extend(utils.use_shelve_file(
                     storage_name=None,
                     func=lambda f: [k for k in f],
                     path=self.storage_path,
-                )
-            else:
-                used_names = []
+                ))
             self.storage_name = self._default_name(names=used_names, extension="")
         else:
+            overwrite = False
             self.storage_name = arg_name
 
         if kwargs.get("save"):
-            self.save(overwrite=True)
+            self.save(overwrite=overwrite)
 
     def __str__(self) -> str:
         return f"""A Timesheet object created {self.creation_time}
@@ -275,6 +325,15 @@ class Timesheet:
         overwrite: bool = False,
         create_directory: bool = False,
     ) -> None:
+        """
+        Save this instance to a :code:`shelve` object at a specified path, using a specified name.
+
+        :param path str: Optional path to the :code:`shelve` file. Default to "$HOME/.timesheet/timesheets".
+        :param storage_name str: Optional name for this instance in :code:`shelve` storage. Generated automatically if omitted.
+        :param overwrite bool: Optional :code:`bool` indicating whether to overwrite an existing instance that shares :code:`storage_name`. Default :code:`False` 
+        :param create_directory bool: Optional logical indivating whether to create the directory containing :code:`storage_path` if it does not exist. Default :code:`False`.
+        :rtype None: 
+        """
         storage_name = self.storage_name if storage_name is None else storage_name
         path = self.storage_path if path is None else path
         target = dirname(path)
@@ -292,7 +351,7 @@ class Timesheet:
 
         with shelve.open(path, "c") as f:
             # Create default storage name if unspecified
-            if storage_name is None: 
+            if storage_name is None:
                 storage_name = self._default_name(list(f.keys()))
             # Bail out on attempt to overwrite if overwrite = False
             if (
@@ -312,14 +371,29 @@ class Timesheet:
 
     @staticmethod
     def load(storage_name: str, storage_path: str = None) -> "Timesheet":
+        """
+        Load a :code:`Timesheet` instance from storage.     
+
+        :param storage_name str: Name of the target instance in :code:`shelve` storage. 
+        :param storage_path str: Path to :code:`shelve` file.
+        :rtype "Timesheet": Instance loaded from the target name and path.
+        """
         return utils.use_shelve_file(
             storage_name=storage_name, func=lambda f: f[storage_name], path=storage_path
         )
 
     @staticmethod
     def delete(
-        storage_name: str, storage_path: str = None, confirm: bool = True
+        storage_name: str, storage_path: str, confirm: bool = True
     ) -> None:
+        """
+        Delete the :code:`Timesheet` instance identified by :code:`storage_path` and :code:`storage_name`.                          
+
+        :param storage_name str: String identifying the target instance in :code:`shelve` storage.
+        :param storage_path str: 
+        :param confirm bool: Optional :code:`bool`indicating whether to ask for confirmation before deletion (if run interactively) or abort (if not). Default :code:`True` 
+        :rtype None: [TODO:description]
+        """
         confirm_prompt = (
             "Press enter to confirm deletion, any other key to abort"
             if confirm
@@ -334,7 +408,12 @@ class Timesheet:
 
     @staticmethod
     def list(path: str = None) -> List[str]:
-        """List names of DayLog files in shelve storage"""
+        """
+        List the names of :code:`Timesheet` instances stored at a particular path   
+
+        :param path str: Path to the :code:`shelve` file to list
+        :rtype List[str]: List of keys to the :code:`shelve` file chosen.  
+        """
         return utils.use_shelve_file(
             path=path,
             func=lambda f: [k for k, v in f.items() if isinstance(v, Timesheet)],
@@ -376,11 +455,10 @@ class Timesheet:
     def __len__(self) -> int:
         return len(self.record)
 
-    def _default_name(self, names: List[str] , extension: str = "") -> str:
+    def _default_name(self, names: List[str], extension: str = "") -> str:
         """Generate a default path or storage name for saving when none is provided"""
         stem = self.__class__.__name__.lower()
         return f"{stem}{utils.next_number(stem = stem, names = names)}{extension}"
-
 
     def write_json(self, path: str = None) -> None:
         if path is None:
@@ -394,49 +472,47 @@ class Timesheet:
 
     def summarize(
         self,
-        start_date: Union[datetime.date, str] = datetime.date.min ,
-        end_date: Union[datetime.date, str]  = datetime.date.max,
-        aggregate: TimeAggregate.TimeAggregate = TimeAggregate.Day
+        start_date: Union[datetime.date, str] = datetime.date.min,
+        end_date: Union[datetime.date, str] = datetime.date.max,
+        aggregate: TimeAggregate.TimeAggregate = TimeAggregate.Day,
     ) -> Dict[str, float]:
         """Sum hours worked for a given week"""
 
-        # Substitute 
-        
+        # Substitute
+
         # Weeks start on Monday, indexed 1
         return utils.sum_DayLogs(
-            start_date=start_date, end_date = end_date, aggregate=aggregate, record=self.record
+            start_date=start_date,
+            end_date=end_date,
+            aggregate=aggregate,
+            record=self.record,
         )
 
     def write_json_summary(
         self,
         json_path: str,
         start_date: Union[datetime.date, str] = datetime.date.min,
-        end_date  : Union[datetime.date, str] = datetime.date.max,
+        end_date: Union[datetime.date, str] = datetime.date.max,
         aggregate: TimeAggregate.TimeAggregate = TimeAggregate.Day,
     ) -> None:
         """Compute summary and save as JSON instead of returning a dict"""
-        summary = self.summarize(start_date=start_date, end_date = end_date, aggregate=aggregate)
+        summary = self.summarize(
+            start_date=start_date, end_date=end_date, aggregate=aggregate
+        )
         with open(json_path, "w") as f:
             json.dump(summary, f)
 
     def write_csv_summary(
-        self, path: str,
+        self,
+        path: str,
         start_date: Union[datetime.date, str] = datetime.date.min,
-        end_date  : Union[datetime.date, str] = datetime.date.max,
-        aggregate: TimeAggregate.TimeAggregate = TimeAggregate.Day
+        end_date: Union[datetime.date, str] = datetime.date.max,
+        aggregate: TimeAggregate.TimeAggregate = TimeAggregate.Day,
     ) -> None:
         # TODO: allow aggregation of total hours worked by week, in addition to targeting just one week
-        # Maybe use a generator to yield week summaries on demand?
-        # data = self.summarize(date = )
-        # Get hours worked for every day recorded
-        # TODO aggregate by year or month
-        #datestamps = [datetime.date.fromisoformat(ts) for ts in self.record.keys()]
-        #start_date = min(datestamps)
         # Add explicit 0s for unrecorded dates bettwen start and end
         data = self.summarize(
-            start_date=start_date,
-            end_date = end_date,
-            aggregate=aggregate
+            start_date=start_date, end_date=end_date, aggregate=aggregate
         )
 
         # Write CSV
@@ -445,7 +521,6 @@ class Timesheet:
             writer = csv.writer(f)
             for k, v in data.items():
                 writer.writerow([k, v])
-
 
     @classmethod
     def from_json(
